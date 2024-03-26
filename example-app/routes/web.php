b059f4719ed06b14e060640d9667459e0b88c4ab
RyanMaz1 <129517784+RyanMaz1@users.noreply.github.com>
2024-03-25 21:24:28 -0300

    first commit

---
+++ example-app/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Admin\SupportController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+
+Route::get('/', function () {
+    return view('welcome');
+});
