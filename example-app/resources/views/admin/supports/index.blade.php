698c07cd7f49e730aacac17f84632f4efc323ac5
RyanMaz1 <129517784+RyanMaz1@users.noreply.github.com>
2024-03-26 15:58:23 -0300

    aula 11

--- example-app/resources/views/admin/supports/index.blade.php
+++ example-app/resources/views/admin/supports/index.blade.php
@@ -1,3 +1,22 @@
 <h1>Listagens do Suportes</h1>
 
 <a href="{{ route('supports.create') }}">Criar Dúvida</a>
+
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Descrição</th>
+        <th></th>
+    </thead>
+    @foreach ($supports as $support)
+        <tr>
+            <td>{{ $support->subject }}</td>
+            <td>{{ $support->status }}</td>
+            <td>{{ $support->body }}</td>
+            <td>
+                >
+            </td>
+        </tr>
+    @endforeach
+</table>
