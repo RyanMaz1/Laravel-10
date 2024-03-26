095c3487c79f9d63cf58ba554920ffd5aab52eab
RyanMaz1 <129517784+RyanMaz1@users.noreply.github.com>
2024-03-26 16:45:09 -0300

    aula 12

--- example-app/resources/views/admin/supports/create.blade.php
+++ example-app/resources/views/admin/supports/create.blade.php
@@ -2,7 +2,7 @@
 
 <form action="{{ route('supports.store') }}" method="post">
     {{-- <input type="hidden" value="{{ csrf_token() }}" name="_token"> --}}
-    @csrf_token
+    @csrf()
     <input type="text" placeholder="Assunto" name="subject">
     <textarea name="body" placeholder="Descrição" cols="30" rows="10"></textarea>
     <button type="submit">Enviar</button>
